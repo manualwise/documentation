a63369cdf29ffd974132a958850374f922b2d9ed
Marko Locher <marko@codeship.com>
2015-10-27 13:02:59 -0400

    First version of a way to include folders from remote repositories
    * Configuration is done via a JSON file
    * Requires JQ to be installed
    * Does not work on Docker based builds yet
    * No automated tests at the moment.

---
+++ bin/includes.sh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+set -e
+
+# switch to the toor of the directory
+DIR="$(cd "$(dirname "${0}")/../" && pwd)"
+cd "${DIR}"
+
+# helper functions
+function log() { echo -e "\e[36m$@\e[39m"; }
+
+# cleanup at the end of the script
+function on_exit() {
+	local exit_code=$?
+	rm -rf '.tmp'
+	exit ${exit_code}
+}
+trap on_exit EXIT
+
+rm -rf '.tmp' && mkdir -p '.tmp'
+jq -cr '.[]' _includes.json | while read key; do
+	url=$(echo "${key}" | jq -r '.repository')
+	name=$(echo "${key}" | jq -r '.repository | @sh')
+	branch=$(echo "${key}" | jq -r '.branch // "master"')
+	source_folder=$(echo "${key}" | jq -r '.source')
+	dest_folder=$(echo "${key}" | jq -r '.destination')
+
+	log "Copying ${source_folder} from repository ${url} (branch ${branch}) to ${dest_folder}"
+	if [ ! -d ".tmp/${name}" ]; then
+		git clone "${url}" ".tmp/${name}"
+	fi
+	cd ".tmp/${name}"
+	git checkout "${branch}"
+	cp -r "./${source_folder}" "${DIR}/${dest_folder}"
+	cd "${DIR}"
+done
