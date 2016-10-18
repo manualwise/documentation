7009d968e10174db369a9fdebe497cc834acb290
Marko Locher <marko@codeship.com>
2016-10-18 12:39:28 +0200

    Better error messages around Jekyll deprecations

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 set -e
-log() { echo -e "\033[36m$@\033[39m"; }
+log() { echo -e "\033[36m$*\033[39m"; }
+fail() { echo -e "\033[31m$*\033[39m"; exit 1; }
 
 # Special settings for the "master" branch, compile the site to the "master"
 # subdirectory, but use an empty baseurl, so we can deploy to the bucket root
@@ -36,5 +37,8 @@ sed -i'' -e "s|^baseurl:.*|baseurl: ${baseurl}|" _config.yml
 export JEKYLL_ENV
 bundle exec jekyll build --destination "${destination}" 2>&1| tee build.log
 
-# Fail if the build log from Jekyll contains any Deprecations
-! cat build.log | grep "Deprecation" -c
+# Fail if the build log from Jekyll contains any deprecations
+deprecations=$(cat build.log | grep "Deprecation" -c)
+if [ ${deprecations} -gt 0 ]; then
+	fail "Found ${deprecations} deprecation warnings, please fix those first."
+fi
